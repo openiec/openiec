02ba264ab3f00712837ee4a1a0999dca924ecef0
shenglan <1472520277@qq.com>
2020-07-06 18:12:49 +0800

    Add examples for the calculation of composition- and temperature-dependent interfacial energies.

---
+++ demo/ComTemDepSigma/NiCr/bin_solliq_NiCr_ctd.py
@@ -0,0 +1,73 @@
+from openiec.calculate.calcsigma import SigmaPure, SigmaSolLiq
+from openiec.property.molarvolume import MolarVolume
+from pycalphad import Database
+import numpy as np
+
+
+def test():
+    """
+    Calculate solid/liquid interfacial energies of the Ni-Cr system.
+    """
+    # Read the data of compisitons and ttemperatures from a .txt file.
+    data = np.genfromtxt("NiCr-xcr-tem.txt")
+    m, n = data.shape
+
+    # Create a .txt file to store the calculated result.
+    output = open("./sigma-NiCr.txt", "w")
+    for i in range(m):
+        # Given temperature.
+        T = data[i][1]
+        # Given initial alloy composition. x0 is the mole fraction of Cr.
+        x0 = [data[i][0]]
+        # Render thermodynamic database.
+        db = Database("NiAlCrHuang1999.tdb")
+        # Define components in the interface.
+        comps = ["NI", "CR", "VA"]
+        # Two phases separated by the interface.
+        phasenames = ["FCC_A1", "LIQUID"]
+
+        # Molar volumes of pure components to construct corresponding molar volume database.
+        # Molar volume of Ni.
+        vni = "6.718*10.0**(-6.0) + (2.936*10.0**(-5)*10.0**(-6.0))*T**1.355"
+        # Molar volume of Cr.
+        vcr = "7.23*10.0**(-6.0)"
+        purevms = [[vni, vcr], [vni, vcr]]
+
+        # A composition range for searching initial interfacial equilirium composition.
+        limit = [10 ** (-20), 0.6]
+        # The composition step for searching initial interfacial equilirium composition.
+        dx = 0.01
+
+        # Call the module for calculating solid/liquid interfacial energies.
+        sigma = SigmaSolLiq(
+            T=T,
+            x0=x0,
+            db=db,
+            comps=comps,
+            phasenames=phasenames,
+            purevms=purevms,
+            limit=limit,
+            dx=dx,
+        )
+        
+        # The given initial alloy composition
+        InitialAlloyCompositionValue = sigma.Initial_Alloy_Composition.values
+        # The calculated interfacial composition.
+        InterfacialCompositionValue = sigma.Interfacial_Composition.values
+        # The calculated partial interfacial energy values.
+        PartialSigmaValue = sigma.Partial_Interfacial_Energy.values
+        # The calculated interfacial energy value.
+        SigmaValue = sigma.Interfacial_Energy.values
+
+        # Write the calculated result into the created file.
+        output.write("%s%s" % ("%.12e\t"*8, "\n") % (
+            T, 
+            InitialAlloyCompositionValue[0], InitialAlloyCompositionValue[1], 
+            InterfacialCompositionValue[0], InterfacialCompositionValue[1],
+            PartialSigmaValue[0], PartialSigmaValue[1],
+            SigmaValue
+        ))
+    output.close()
+
+if __name__ == "__main__":
+    test()
